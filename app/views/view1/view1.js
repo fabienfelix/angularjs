250bff0439bd17482083a1d8486f14ebb071e174
Fabien Felix <fafelix-ext@airfrance.fr>
2016-11-23 17:49:56 +0100

    Add functions and cookies

--- app/views/view1/view1.js
+++ app/views/view1/view1.js
@@ -1,14 +1,43 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ngCookies'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'views/view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+    $routeProvider.when('/view1', {
+        templateUrl: 'views/view1/view1.html',
+        controller: 'View1Ctrl'
+    });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$cookies', '$cookieStore', function($scope, $cookies, $cookieStore) {
+
+    $scope.test = 0;
+
+    $scope.view = {
+        'title': 'View 1'
+    };
+
+    $scope.someArray = [{
+        'id': 1,
+        'name': 'view 1'
+    }, {
+        'id': 2,
+        'name': 'view 2'
+    }, {
+        'id': 3,
+        'name': 'Deb'
+    }];
+
+    $scope.myFuncTest = function() {
+        $scope.test = $cookieStore.get('listName');
+    };
+
+    $scope.myFunc = function(id, name) {
+        // $scope.test = id;
+        $cookieStore.put('listId', id);
+        $cookieStore.put('listName', name);
+    };
+
+
 
 }]);
